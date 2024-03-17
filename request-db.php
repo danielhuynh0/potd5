53d0f4b412fcb992a77c031d3930bb1000ce7918
HarshalNallapareddy <harshalreddyn@gmail.com>
2024-03-17 12:31:49 -0400

    populated fucntions

--- request-db.php
+++ request-db.php
@@ -1,32 +1,34 @@
 <?php
 function addRequests($reqDate, $roomNumber, $reqBy, $repairDesc, $reqPriority)
 {
-
-
+    $stmt = $db->prepare("INSERT INTO requests(reqDate, roomNumber, reqBy, repairDesc, reqPriority) VALUES (?, ?, ?, ?, ?)");
+    $stmt->execute([$reqDate, $roomNumber, $reqBy, $repairDesc, $reqPriority]);
 }
 
 function getAllRequests()
 {
-   
-
+    $stmt = $db->query("SELECT * FROM requests");
+    return $stmt->fetchAll();
 }
 
 function getRequestById($id)  
 {
-    
-
+    $stmt = $db->prepare("SELECT * FROM requests WHERE reqId = ?");
+    $stmt->execute([$id]);
+    return $stmt->fetch();
 }
 
 function updateRequest($reqId, $reqDate, $roomNumber, $reqBy, $repairDesc, $reqPriority)
 {
-
-
+    $stmt = $db->prepare("UPDATE requests SET reqDate = ?, roomNumber = ?, reqBy = ?, repairDesc = ?, reqPriority = ? WHERE reqId = ?");
+    $stmt->execute([$reqDate, $roomNumber, $reqBy, $repairDesc, $reqPriority, $reqId]);
 }
 
 function deleteRequest($reqId)
 {
-
-    
+    $stmt = $db->prepare("DELETE FROM requests WHERE reqId = ?");
+    $stmt->execute([$reqId]);    
 }
 
 ?>
+
